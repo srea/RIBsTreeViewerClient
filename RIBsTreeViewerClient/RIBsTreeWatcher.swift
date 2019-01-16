162b0b899cc239a58276763d65d4556da3e995c3
yuki tamazawa <y.tamazawa@japantaxi.co.jp>
2019-01-16 16:42:36 +0900

    Add Initial Project

---
+++ RIBsTreeViewerClient/RIBsTreeWatcher.swift
@@ -0,0 +1,13 @@
+//
+//  RIBsTreeWatcher.swift
+//  RIBsTreeViewerClient
+//
+//  Created by 玉澤　裕貴 on 2019/01/16.
+//  Copyright © 2019 minipro. All rights reserved.
+//
+
+import Foundation
+
+final public class RIBsTreeWatcher {
+
+}
