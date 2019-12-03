0ab2d0888519fe023cb0b202ee67e6290055c768
yuki tamazawa <y.tamazawa@japantaxi.co.jp>
2019-12-03 23:38:09 +0900

    [ci skip]  Add WebSocketServer & Browser Code

---
+++ Browser/src/app.js
@@ -0,0 +1,33 @@
+import 'jqtree'
+import * as blobUtil from 'blob-util'
+
+const sock = new WebSocket("ws://0.0.0.0:8080");
+
+sock.addEventListener("message", e => {
+    if (typeof e.data === "string") {
+        try {
+            let parsedJSON = JSON.parse("[" + e.data + "]");
+            $('#tree').remove()
+            $("<div>", {
+                id: 'tree'
+            }).appendTo('#body');
+            $('#tree').tree({
+                data: parsedJSON,
+                autoOpen: true,
+                onCreateLi: function (node, $li) {
+                    $li.find('.jqtree-element').click(function () {
+                        sock.send(node.name.replace(" (View) ", ""));
+                        return false;
+                    });
+                }
+            });
+        } catch (e) {
+        }
+    } else {
+        const blob = new Blob([e.data], {
+            type: 'image/jpeg'
+        });
+        var blobURL = blobUtil.createObjectURL(blob);
+        document.getElementById('capture').setAttribute('src', blobURL);
+    }
+});
---
+++ Browser/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public/js')
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+  ]
+};
