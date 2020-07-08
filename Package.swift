fc7c6f2fea9262d4635b902543f611c40ba34ab0
Yuki Tamazawa <yuki.tamazawa@icloud.com>
2020-07-08 15:49:32 +0900

    update package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,17 @@ let package = Package(
     name: "RIBsTreeViewerClient",
     platforms: [.iOS(.v13)],
     products: [
-            .library(name: "RIBsTreeViewerClient", targets: ["RIBsTreeViewerClient"]),
-        ],
-        dependencies: [
-            .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.0"),
-            .package(url: "https://github.com/uber/RIBs.git", from: "0.9.2"),
-        ],
-        targets: [
+        .library(name: "RIBsTreeViewerClient", targets: ["RIBsTreeViewerClient"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.1.0"),
+        .package(url: "https://github.com/uber/RIBs.git", from: "master"),
+    ],
+    targets: [
         .target(
             name: "RIBsTreeViewerClient",
-            path: "./RIBsTreeViewerClient/Sources",
-            dependencies: ["RxSwift", "RIBs"]
-        ),
+            dependencies: ["RxSwift", "RIBs"],
+            path: "./RIBsTreeViewerClient/Sources"
+        )
     ]
 )
